27bfc974403c6cd1aad33da88fd16c932989e36e
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-02-15 17:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function minCostToFormRope(event) {
+	event.preventDefault();
+	// we will get as string
+	var inputElement = document.querySelector("input").value;
+	// break the string into array
+	var arr = inputElement.split(',');
+	arr.sort((a, b) => (a-b));
+
+	var cost = 0;
+	while(arr.length > 1){
+		var res = Number(arr[0]) + Number(arr[1]);
+		cost += res;
+		arr.splice(0,2);
+		arr.push(res);
+		arr.sort((a, b) => (a-b));
+	}
+	document.getElementById("result").textContent = cost;
+}
